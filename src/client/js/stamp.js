86f04ddd51c2085e657a81e575d44b918e48ee4b
김진실 <kimjinsil@dfit4620.local>
2023-10-16 15:48:18 +0900

    스탬프 갯수따라 미션완료 쿠폰 뜨도록 연동 완료

--- src/client/js/stamp.js
+++ src/client/js/stamp.js
@@ -37,6 +37,9 @@ const missionComplete = () => {
     const missionBox = document.getElementById("mission_box");
     const couponBox = document.getElementById("coupon_box");
 
+    const userData  = stampListInfo.map((draw_lots_list) => draw_lots_list.drawLotsList_id)
+    // console.log(userData)
+
     const stamp_visited  = stampListInfo.filter((users_stamp) => users_stamp.stamp_level === 1)
     const stamp_experienced  = stampListInfo.filter((users_stamp) => users_stamp.stamp_level === 2)
     const stamp_completed = stamp_visited.length + stamp_experienced.length*2
@@ -46,12 +49,21 @@ const missionComplete = () => {
     let couponHtml = "";
 
     if(stamp_completed >= 6){
-        //미션완료
-        missionHtml += `<div class="w-[160px] h-[40px] flex justify-center items-center bg-[#FFAA2C]">`
-        missionHtml += `쿠폰받기`
-        missionHtml += `</div>`
-
-        // completeHtml +=`<div class=""><img src="../file/stamp/mission_complete.png"></div>`
+        if(userData[0]===null){
+            //미션완료
+            couponBox.classList.add('hidden');
+            missionHtml += `<div onclick="clickGetCoupon()" class="w-[160px] h-[40px] flex justify-center items-center bg-[#FFAA2C]">`
+            missionHtml += `쿠폰받기`
+            missionHtml += `</div>`
+        }else{
+            //쿠폰보기
+            missionBox.classList.add('hidden');
+            couponHtml += `<button 
+            data-modal-target="defaultModal" data-modal-toggle="defaultModal" type="button" 
+            class="w-[160px] h-[40px] flex justify-center items-center bg-[#292929] text-white">`
+            couponHtml += `쿠폰 보기`
+            couponHtml += `</button>`
+        }
     }else{
         missionHtml += `<div id="stamp_count_box" class="flex justify-center items-center w-[164px] h-[41px] border border-gray font-bold bg-[#c8c8c8] text-[#333333] text-[16px] rounded tracking-wide">`
         missionHtml += `<i class="fas fa-stamp"></i>`
@@ -63,7 +75,10 @@ const missionComplete = () => {
     couponBox.innerHTML = couponHtml;
 }
 
-
+const clickGetCoupon = () => {
+    missionCompleteListFetch();
+    location.reload(); return;
+}
 
 
 //쿠폰보기
@@ -91,7 +106,7 @@ const stampCount = () => {
     console.log(stamp_completed);
 }   
 
-//백엔드 서버로 스탬프정보 요청
+//백엔드 서버로 스탬프 리스트 정보 요청
 const getStampListFetch = async () => {
     const accessToken = localStorage.getItem("accessToken");
     const response = await fetch("/api/stamp",{
@@ -107,7 +122,7 @@ const getStampListFetch = async () => {
 
     if (response.status === 200) {
       console.log("getStampList api 연동 성공");
-      console.log(stampListInfo)
+    //   console.log(stampListInfo)
       makeStampHtml(); 
       missionComplete(); 
     } else {
@@ -115,7 +130,7 @@ const getStampListFetch = async () => {
     }
   };
 
-  //백엔드 서버로 스탬프정보 요청
+  //백엔드 서버로 스탬프 갯수 정보 요청
 const getStampCountFetch = async () => {
     const accessToken = localStorage.getItem("accessToken");
     const response = await fetch("/api/stamp",{
@@ -128,7 +143,6 @@ const getStampCountFetch = async () => {
     });
     const result = await response.json();
     stampCountInfo = result;
-    console.log(stampCountInfo)
 
     if (response.status === 200) {
       console.log("getStampCount api 연동 성공");
@@ -139,7 +153,7 @@ const getStampCountFetch = async () => {
     }
   };
 
-  //백엔드 서버로 스탬프정보 요청
+//   스탬프 미션 완료 => 미션완료 데이터 서버에 넣기
 const missionCompleteListFetch = async () => {
     const accessToken = localStorage.getItem("accessToken");
     const response = await fetch("/api/stamp/mission",{
@@ -156,7 +170,7 @@ const missionCompleteListFetch = async () => {
 
     if (response.status === 200) {
       console.log("getStampCount api 연동 성공");
-    //   console.log(stampListInfo)
+      clickGetCoupon();
     } else {
       console.log("getStampCount api 연동 에러");
     }
